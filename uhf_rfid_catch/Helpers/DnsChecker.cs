8a8857dc0fd8a75edd1c143d5ab33a673eb8c126
Godwin peter .O <me@godwin.dev>
2020-01-09 14:58:48 +0100

    Fix internet check exception

--- uhf_rfid_catch/Helpers/DnsChecker.cs
+++ uhf_rfid_catch/Helpers/DnsChecker.cs
@@ -6,26 +6,23 @@ namespace uhf_rfid_catch.Helpers
     public class DnsChecker
     {
         private static bool _finalStatus;
+        private static readonly ConfigContext SettingsContext = new ConfigContext();
 
         readonly Ping _pingInit = new Ping();
         private readonly byte[] _buffer = new byte[32];
-        private readonly int _timeout = 1000;
+        private readonly int _timeout = Convert.ToInt32(SettingsContext.Resolve("InternetCheckTimeout"));
         readonly PingOptions _pingOptions = new PingOptions();
-        private static readonly ConfigContext SettingsContext = new ConfigContext();
-        readonly string _host = SettingsContext.Resolve("InternetCheckAddress");
-        
+        private readonly string _host = SettingsContext.Resolve("InternetCheckAddress");
         public DnsChecker()
         {
-//            Console.WriteLine(_host);
             try
             {
                 PingReply receivedPingReply = _pingInit.Send(_host, _timeout, _buffer, _pingOptions);
                 if (receivedPingReply != null) _finalStatus = receivedPingReply.Status == IPStatus.Success;
             }
-            catch (Exception e)
+            catch (Exception)
             {
-                Console.WriteLine(e);
-                throw;
+                _finalStatus = false;
             }
         }
 
