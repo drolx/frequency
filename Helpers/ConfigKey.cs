1d7684431b869b3cd79d449795f223ea820685d7
Godwin peter .O <me@godwin.dev>
2020-01-23 19:12:30 +0100

    Update ConfigKey.cs

--- Helpers/ConfigKey.cs
+++ Helpers/ConfigKey.cs
@@ -40,10 +40,13 @@ namespace uhf_rfid_catch.Helpers
         
         // Port for web view
         public readonly int BASE_WEB_PORT = (int) _settingsContext.GetSection("WebPort").Get<decimal>();
+
         // Option to persist capture data for the daemon.
         public readonly bool BASE_PERSIST_DATA = _settingsContext.GetSection("PersistData").Get<bool>();
+
         // Data persist duration.
         public readonly int BASE_PERSIST_DURATION_DAYS = (int) _settingsContext.GetSection("PersistDurationDays").Get<decimal>();
+
         // Option to allow all, or a list of host.
         public readonly string BASE_ALLOWED_HOSTS = _settingsContext.GetSection("AllowedHosts").Get<string>();
         
@@ -54,6 +57,7 @@ namespace uhf_rfid_catch.Helpers
         
         // Enable or disables logging.
         public readonly bool LOGGING_ENABLE = _settingsContext.GetSection("Logging:Enable").Get<bool>();
+
         // the Default log level for the daemon.
         public readonly string LOGGING_LEVEL = _settingsContext.GetSection("Logging:LogLevel:Default").Get<string>();
         
@@ -68,10 +72,13 @@ namespace uhf_rfid_catch.Helpers
         /// <summary>
         /// Redis distributed caching configurations.
         /// </summary>
+        
         // Redis hostname or IP address information.
         public readonly string DATA_REDIS_HOST = _settingsContext.GetSection("Database:Redis:Host").Get<string>();
+
         // Redis host port
         public readonly int DATA_REDIS_PORT = (int) _settingsContext.GetSection("Database:Redis:Port").Get<decimal>();
+
         // Redis instance type, either master or slave.
         public readonly string DATA_REDIS_INSTANCE = _settingsContext.GetSection("Database:Redis:Instance").Get<string>();
         
@@ -85,8 +92,10 @@ namespace uhf_rfid_catch.Helpers
         
         // Option to enable or disable server mode.
         public readonly bool SERVER_ENABLE = _settingsContext.GetSection("ServerMode:Enable").Get<bool>();
+
         // Option to allow option to save captured data to the store in server mode.
         public readonly bool SERVER_STORE = _settingsContext.GetSection("ServerMode:Store").Get<bool>();
+
         // Get Configured protocols list.
         public readonly IEnumerable SERVER_PROTOCOLS = _settingsContext.GetList("ServerMode:Protocols");
         
@@ -97,30 +106,43 @@ namespace uhf_rfid_catch.Helpers
         
         // Option to enable or disable IOT remote functionality.
         public readonly bool IOT_MODE_ENABLE = _settingsContext.GetSection("IOTMode:Enable").Get<bool>();
+
         // Option for how many simultaneous HTTP push can happen at once.
         public readonly int PUSH_PROCESS_COUNT = (int) _settingsContext.GetSection("IOTMode:PushProcessCount").Get<decimal>();
+
         // The IOT mode unique identity for the daemon instance.
         public readonly string IOT_UNIQUE_ID = _settingsContext.GetSection("IOTMode:UniqueId").Get<string>();
+
         // IOT mode required protocol to decode.
         public readonly string IOT_PROTOCOL = _settingsContext.GetSection("IOTMode:Protocol").Get<string>();
+
         // Remote Host URI.
         public readonly string IOT_REMOTE_HOST_URL = _settingsContext.GetSection("IOTMode:RemoteHostUrl").Get<string>();
+
         // Remote Host HTTP call method.
         public readonly string IOT_REMOTE_HOST_METHOD = _settingsContext.GetSection("IOTMode:RemoteHostMethod").Get<string>();
+
         // Remote Host HTTP authentication username.
         public readonly string IOT_REMOTE_HOST_USERNAME = _settingsContext.GetSection("IOTMode:RemoteHostUsername").Get<string>();
+
         // Remote Host HTTP authentication password.
         public readonly string IOT_REMOTE_HOST_PASSWORD = _settingsContext.GetSection("IOTMode:RemoteHostPassword").Get<string>();
+
         // Option for minimum delay in seconds that allowed for an HTTP call.
         public readonly int IOT_MIN_REMOTE_HOST_DELAY = (int) (1000 * _settingsContext.GetSection("IOTMode:MinRemoteHostDelay").Get<decimal>());
+
         // Option for how long a card is disallowed from generation a HTTP push after an entry.
         public readonly int IOT_MIN_REPEAT_FREQ = (int) (1000 * _settingsContext.GetSection("IOTMode:MinRepeatFrequency").Get<decimal>());
+
         // Options to enable or disable auto read mode.
         public readonly bool IOT_AUTO_READ = _settingsContext.GetSection("IOTMode:AutoRead").Get<bool>();
+
         // Options to enable or disable auto read mode.
         public readonly bool IOT_NETWORK_CHECK = _settingsContext.GetSection("IOTMode:NetworkCheck").Get<bool>();
+
         // Options to enable or disable auto read mode.
         public readonly string IOT_NETWORK_CHECK_ADDRESS = _settingsContext.GetSection("IOTMode:NetworkCheckAddress").Get<string>();
+
         // Options to configure maximum network connection timeout in seconds.
         public readonly int IOT_NETWORK_CHECK_TIMEOUT = (int) _settingsContext.GetSection("IOTMode:NetworkCheckTimeout").Get<decimal>();
         
