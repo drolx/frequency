20aa50a13432e21712efce7afd0150ae43f59656
Godwin peter .O <me@godwin.dev>
2020-01-30 16:12:16 +0100

    Logging handler optimization

--- Handlers/MainLogger.cs
+++ Handlers/MainLogger.cs
@@ -24,6 +24,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 using System;
+using System.IO;
 using NLog;
 
 namespace uhf_rfid_catch.Handlers
@@ -34,14 +35,34 @@ namespace uhf_rfid_catch.Handlers
         void Error(String returnText);
         void Warn(String returnText);
         void Debug(String returnText);
+        void Trigger(string logType, string returnText);
     }
 
     public class MainLogger : IMainLogger
     {
-        private static Logger _logger = LogManager.GetCurrentClassLogger();
+        private static Logger _logger;
 
         public MainLogger()
         {
+            string LogFilePath;
+            if (File.Exists("nlog.config"))
+            {
+                LogFilePath = "nlog.config";
+            }
+            else if (File.Exists("NLog.config"))
+            {
+                LogFilePath = "NLog.config";
+            }
+            else if (File.Exists("uhf_rfid_catch.exe.nlog"))
+            {
+                LogFilePath = "uhf_rfid_catch.exe.nlog";
+            }
+            else
+            {
+                LogFilePath = "";
+                Console.WriteLine("No Log configuration found..");
+            }
+            _logger = NLog.Web.NLogBuilder.ConfigureNLog(LogFilePath).GetCurrentClassLogger();
         }
 
         public void Trigger(String logType, String returnText)
