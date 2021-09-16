9dc47b84b080405a9014d5c1fa14e7e665296dc2
radioActive DROID <me@godwin.dev>
2021-09-16 16:54:08 +0100

    Enable nullable and switch to minimal API

--- Program.cs
+++ Program.cs
@@ -1,70 +1,20 @@
-﻿//
-// Program.cs
-//
-// Author:
-//       Godwin peter .O <me@godwin.dev>
-//
-// Copyright (c) 2020 MIT
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Hosting;
-using NLog.Web;
-using Iot.Rfid.Handlers;
-using Iot.Rfid.Helpers;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
 
-
-namespace Iot.Rfid
+var app = builder.Build();
+/**
+* Configure the HTTP request pipeline.
+**/
+if (!app.Environment.IsDevelopment())
 {
-    internal static class Program
-    {
-        private static readonly ConfigKey _config = new ConfigKey();
-        private static readonly MainLogger _logger = new MainLogger();
-        private static readonly ReaderProcess _readerProcess = new ReaderProcess();
-        
-        static void Main(string[] args)
-        {
-            Console.WriteLine(Figgle.FiggleFonts.Standard.Render("UHFRFID IOT"));
-
-            _logger.Trigger("Info", "Booting up daemon....");
-            
-            // Reader process thread//
-            Task.Run(() => _readerProcess.Run()).Wait();
-            
-            // Web view thread//
-            if (_config.BASE_WEB_ENABLE)
-            {
-                Task.Run(() => CreateHostBuilder(args).Build().Run()).Wait();
-            }
-        }
-
-        private static IHostBuilder CreateHostBuilder(string[] args) =>
-    Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); })
-        .ConfigureLogging(logging =>
-        {
-            logging.ClearProviders();
-            logging.SetMinimumLevel(LogLevel.Trace);
-        })
-        .UseNLog();  // NLog: Setup NLog for Dependency injection
-        }
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapRazorPages();
+
+app.Run();
