f9746a6c38ee4d22e9ebf1b50be54fc7a50d6efe
radioActive DROID <me@godwin.dev>
2021-09-19 20:51:21 +0100

    Revert to old namespace type

--- src/Iot.Rfid/Pages/Error.cshtml.cs
+++ src/Iot.Rfid/Pages/Error.cshtml.cs
@@ -1,13 +1,14 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace Iot.Rfid.Pages;
-
-[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-[IgnoreAntiforgeryToken]
-public class ErrorModel : PageModel
+namespace Iot.Rfid.Pages
 {
+  [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+  [IgnoreAntiforgeryToken]
+  public class ErrorModel : PageModel
+  {
     public string? RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
@@ -16,12 +17,12 @@ public class ErrorModel : PageModel
 
     public ErrorModel(ILogger<ErrorModel> logger)
     {
-        _logger = logger;
+      _logger = logger;
     }
 
     public void OnGet()
     {
-        RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+      RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
     }
+  }
 }
-
--- src/Iot.Rfid/Pages/Index.cshtml.cs
+++ src/Iot.Rfid/Pages/Index.cshtml.cs
@@ -1,19 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 
-namespace Iot.Rfid.Pages;
-
-public class IndexModel : PageModel
+namespace Iot.Rfid.Pages
 {
+  public class IndexModel : PageModel
+  {
     private readonly ILogger<IndexModel> _logger;
 
     public IndexModel(ILogger<IndexModel> logger)
     {
-        _logger = logger;
+      _logger = logger;
     }
 
     public void OnGet()
     {
 
     }
+  }
 }
