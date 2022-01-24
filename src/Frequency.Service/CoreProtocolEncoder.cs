e7612e45855b3be7359c821c3bb85f5617eda08a
radioActive DROID <me@godwin.dev>
2022-01-24 03:13:29 +0100

    Random..

--- src/Frequency.Service/CoreProtocolEncoder.cs
+++ src/Frequency.Service/CoreProtocolEncoder.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Proton.Frequency.Service;
 
-namespace Proton.Frequency.Service
+internal class CoreProtocolEncoder
 {
-    internal class CoreProtocolEncoder
-    {
-    }
 }
