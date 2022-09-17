fef1b1619f37430568ae0cc9e990bb92c797f538
radioActive DROID <me@godwin.dev>
2022-09-18 00:05:01 +0100

    Update Program.cs

--- src/Proton.Frequency/Program.cs
+++ src/Proton.Frequency/Program.cs
@@ -2,14 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Configuration
     .AddYamlFile("config.main.yaml", optional: false, reloadOnChange: true)
     .AddYamlFile("config.serial.yaml", optional: false, reloadOnChange: true)
-    .AddYamlFile("config.network.yaml", optional: false, reloadOnChange: true)
-    .AddEnvironmentVariables();
+    .AddYamlFile("config.network.yaml", optional: false, reloadOnChange: true);
 builder.Services.AddRazorPages();
 var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
+    app.UseHsts();
 }
+app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
