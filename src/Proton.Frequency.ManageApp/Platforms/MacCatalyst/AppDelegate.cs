06a9e0fdda30a0921f42f841473111c22e4e3cad
radioActive DROID <me@godwin.dev>
2022-10-07 19:21:47 +0100

    Added native management client app template

---
+++ src/Proton.Frequency.ManageApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Proton.Frequency.ManageApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
