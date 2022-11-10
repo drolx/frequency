b966b9c6cde1cb454b0b584a6979a1f2fd3b7afb
radioActive DROID <me@godwin.dev>
2022-11-10 22:17:15 +0100

    Restructure solutions

---
+++ src/ManageApp/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Proton.Frequency.ManageApp {
+    public partial class App : Application {
+        public App() {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ src/ManageApp/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using UraniumUI;
+
+namespace Proton.Frequency.ManageApp {
+    public static class MauiProgram {
+        public static MauiApp CreateMauiApp() {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
+                .UseMauiCommunityToolkitMarkup()
+                .ConfigureFonts(fonts => {
+                    fonts.AddFontAwesomeIconFonts();
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+            builder.Services.AddSingleton<App>();
+
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
