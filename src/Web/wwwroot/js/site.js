588e0035abd4628729d8ba326195b33280c61465
radioActive DROID <me@godwin.dev>
2022-11-23 09:54:22 +0100

    Update ObjectDecoder.cs, Proton.Frequency.csproj, and 3 more files...

--- src/Web/wwwroot/js/site.js
+++ src/Web/wwwroot/js/site.js
@@ -1,17 +1,17 @@
 ﻿/* DEPRECIATED: Old active line hak for Asp.net */
-//function handleActiveNav(targetClass) {
-//    document.addEventListener('DOMContentLoaded', () => {
-//        document.querySelectorAll(targetClass).forEach(link => {
-//            const activePath = location.pathname.toLowerCase();
-//            const currentPath = link.getAttribute('href').toLowerCase();
-//            const root = currentPath.length === 1;
+function handleActiveNav(targetClass) {
+   document.addEventListener('DOMContentLoaded', () => {
+       document.querySelectorAll(targetClass).forEach(link => {
+           const activePath = location.pathname.toLowerCase();
+           const currentPath = link.getAttribute('href').toLowerCase();
+           const root = currentPath.length === 1;
 
-//            if (root && currentPath === activePath) link.classList.add('active');
-//            else if (!root && activePath.includes(currentPath)) link.classList.add('active');
-//            else {
-//                link.classList.remove('active');
-//            }
-//        });
-//    })
-//}
+           if (root && currentPath === activePath) link.classList.add('active');
+           else if (!root && activePath.includes(currentPath)) link.classList.add('active');
+           else {
+               link.classList.remove('active');
+           }
+       });
+   })
+}
 
